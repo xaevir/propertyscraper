3e91e64e3647bfbc96453cd4308af078d1266b6f
bobby <xaevir@gmail.com>
2013-09-16 19:13:19 -0400

    can scrape more than 1 page

--- mattyscrape.js
+++ mattyscrape.js
@@ -1,8 +1,25 @@
 var links = [];
-var casper = require('casper').create();
 var utils = require('utils');
 var fs = require('fs'); 
 var root = 'https://www.taxpayerservicecenter.com/'
+var casper = require("casper").create({
+    clientScripts:[ "jquery.js"],
+    //verbose:true,
+    logLevel: 'info',
+    pageSettings:{
+        loadImages:false,
+        loadPlugins:false,
+        javascriptEnabled:true,
+    }
+});
+
+var hood_id = casper.cli.get('id')
+if (!hood_id) {
+  casper.echo("You need to pass in neighborhood id as argument to this code.");
+  casper.exit();
+}
+
+
 
 function getLinks() {
   var links = document.querySelectorAll('.aLink');
@@ -11,36 +28,43 @@ function getLinks() {
   });
 }
 
-function nextLink() {
-  casper.page.injectJs('jquery.js');
-  this.evaluate(function(){
+function hasNextLink() {
+  var href = this.evaluate(function (){
     var link = $('a:contains("next")')
-    if (link.length !== 0) {
-      casper.click(link)
-      casper.then(function() {
-        links = this.evaluate(getLinks);
-      })
-    }
+    return (link.length !== 0) ? link.attr('href') : false
   });
+  this.log('link href: ' + href, 'warning')
+  return href
 }
 
-casper.start('https://www.taxpayerservicecenter.com/RP_Search.jsp?search_type=Assessment', function() {
+function traverse() {
+  var link = hasNextLink.call(this)
+  if (link) {
+    this.thenOpen(root + link, function() {
+      links = links.concat(this.evaluate(getLinks));
+      this.log(links.length + ' links found:', 'warning');
+      traverse.call(this)
+    })
+  }
+}
+
+casper.start('https://www.taxpayerservicecenter.com/RP_Search.jsp?search_type=Assessment', function start() {
   this.fill('form#SearchForm', {
-    'selectNbhdCode':    '36',
+    'selectNbhdCode':    hood_id,
     'selectUseCode':     '021'
   }, true);
 });
 
-casper.then(function() {
+casper.then(function secondStep() {
   links = this.evaluate(getLinks);
-  //casper.page.injectJs('jquery.js');
-
+  this.log(links.length + ' links found 1:', 'warning');
+  traverse.call(this)
 });
 
 casper.then(function() {
+  var i = 1
   casper.each(links, function(self, link) {
     self.thenOpen(root + link, function() {
-      //console.log('new location is ' + this.getCurrentUrl());
       casper.page.injectJs('jquery.js');
       var data =  this.evaluate(function(){
         var data = {}
@@ -51,22 +75,21 @@ casper.then(function() {
         data.mailing = $('form#form1 td:contains("Mailing Address:")').next().text()
         return data
       });
-      this.echo( data.address + '\t' + data.ssl + '\t' + data.owner + '\t' + data.mailing );
+      this.echo(i + '\t' + data.address + '\t' + data.ssl + '\t' + data.owner + '\t' + data.mailing );
+      i++
     });
   });
 });
 
-
-casper.run(function() {
-  //this.echo(links2.length + ' links found:');
-  //this.echo(' - ' + links2.join('\n - ')).exit();
+casper.on("remote.message", function(message) {
+  this.echo(message);
 });
 
-//this.echo(this.getTitle());
+
+
+casper.run(function() {
+  this.exit(); 
+});
+
 //fs.write(myfile, myData, 'w');
-//return $('font:contains("Address:")');
-//return $('form table tbody tr td table tbody tr td:nth-child(2)').text()
-
-//var address_selector = 'form table tbody tr td table tbody tr td:nth-child(2)'
-//var address_info = this.getElementInfo(address_selector); 
-
+//this.log('new location is ' + this.getCurrentUrl(), 'info');
